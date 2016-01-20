7ee1082a14678f8b02fc582e4cd4511425c84462
Alexander Blinov <alblinov@wiley.com>
2016-01-20 20:25:03 +0300

    add category and objective

--- src/org/spiritlab/hran/model2/Task.java
+++ src/org/spiritlab/hran/model2/Task.java
@@ -1,5 +1,8 @@
 package org.spiritlab.hran.model2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Task {
@@ -9,5 +12,27 @@ public class Task {
     private String title;
     
     private Map<Category, String> properties;
+    
+    private List<Objective> objectives;
+    
+    public Task(String title) {
+        this.title = title;
+    }
+    
+    public Task addProperty(Category category, String value) {
+        if (properties == null) {
+            properties = new HashMap<Category, String>();
+        }
+        properties.put(category, value);
+        return this;
+    }
+    
+    public Task addObjective(Objective objective) {
+        if (objectives == null) {
+            objectives = new ArrayList<Objective>();
+        }
+        objectives.add(objective);
+        return this;
+    }
     
 }
